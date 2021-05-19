708d1fa8dcf6402b5b7011f0ab5b8bc12d1cbb07
D3MON1A <leisyarcia@gmail.com>
2021-05-19 17:40:00 -0500

    updates

---
+++ samirs_help/spark.py
@@ -0,0 +1,33 @@
+from pyspark.context import SparkContext
+from pyspark.sql.session import SparkSession
+
+# spark-submit --packages org.apache.spark:spark-avro_2.11:2.4.4 004sparkTesting.py
+# command to pass.
+
+spark: SparkSession = SparkSession.builder.master("local[1]")\
+.appName("sparkjsonavro")\
+.getOrCreate()
+
+sc = SparkContext
+
+df = spark.read.option("multiline",'true').json("file:///home/consultant/Desktop/BigDataBricks/samirs_help/people.json")
+df.printSchema()
+df.show()
+
+# Write Avro File
+
+df.write.format("parquet")\
+.mode("append") \
+.option("mergeSchema", "true") \
+.save("file:///home/consultant/Desktop/BigDataBricks/Parquet")
+
+
+# Read Avro File
+
+# avroDf = spark.read\
+# .option("inferSchema", "true")\
+# .format("parquet")\
+# .load("/sparkTesting/")
+
+avroDf.printSchema()
+avroDf.show()
