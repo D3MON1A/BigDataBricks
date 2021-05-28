86d90e3eae481c10de33160997c5776871552530
D3MON1A <leisyarcia@gmail.com>
2021-05-27 20:59:25 -0500

    updating

--- 3rd_pipeline/bucket.py
+++ 3rd_pipeline/bucket.py
@@ -1,8 +1,16 @@
 import boto3
 import json
 
-s3 = boto3.resource('s3')
-content_object = s3.Object('myfirstumasteritbucket', 's3://myfirstumasteritbucket/UmasterIT/')
+s3 = boto3.resource('s3',
+                    aws_access_key_id='AKIAY3CWJHSUTXNREK6Y',\
+                    aws_secret_access_key='dI+SpFai6S47INYaGpVi7SOJGVEF2vzos9BNJ0OF')
+
+content_object = s3.Object(bucket_name='myfirstumasteritbucket', key='UmasterIT/sample.json')
 file_content = content_object.get()['Body'].read().decode('utf-8')
 json_content = json.loads(file_content)
-print(json_content['Details'])
+print(json_content)
+
+# mymongo = SparkSession.builder.appName("capstonePlayStore")\
+# .config("spark.mongodb.input.uri", "mongodb://127.0.0.1/test.playstore") \
+# .config("spark.mongodb.output.uri", "mongodb://127.0.0.1/test.playstore") \
+# .getOrCreate()df.write.format("mongo").mode("append").save()
