2459fc848591f0f9e44f84aa89b04aaa8ab66efe
D3MON1A <leisyarcia@gmail.com>
2021-06-02 15:21:44 -0500

    done

--- 3rd_pipeline/bucket.py
+++ 3rd_pipeline/bucket.py
@@ -1,16 +1,38 @@
 import boto3
 import json
+from pyspark.sql import SparkSession
+from pyspark.context import SparkContext
+from pyspark.sql.types import *
+
+
+mymongo = SparkSession.builder.appName("awsbucket")\
+.config("spark.mongodb.input.uri", "mongodb://127.0.0.1/bucket.example") \
+.config("spark.mongodb.output.uri", "mongodb://127.0.0.1/bucket.example") \
+.getOrCreate()
 
 s3 = boto3.resource('s3',
-                    aws_access_key_id='AKIAY3CWJHSUTXNREK6Y',\
-                    aws_secret_access_key='dI+SpFai6S47INYaGpVi7SOJGVEF2vzos9BNJ0OF')
+                    aws_access_key_id='',\
+                    aws_secret_access_key='')
 
 content_object = s3.Object(bucket_name='myfirstumasteritbucket', key='UmasterIT/sample.json')
 file_content = content_object.get()['Body'].read().decode('utf-8')
 json_content = json.loads(file_content)
-print(json_content)
 
-# mymongo = SparkSession.builder.appName("capstonePlayStore")\
-# .config("spark.mongodb.input.uri", "mongodb://127.0.0.1/test.playstore") \
-# .config("spark.mongodb.output.uri", "mongodb://127.0.0.1/test.playstore") \
-# .getOrCreate()df.write.format("mongo").mode("append").save()
+
+sc= SparkContext.getOrCreate()
+
+# Convert list to RDD
+rdd = sc.parallelize(json_content).map(lambda x :(x['id'],x['type']))
+
+schema = StructType([\
+StructField("id", StringType(), True),\
+StructField("type", StringType(), True)])
+
+
+# Create data frame
+df = mymongo.createDataFrame(rdd, schema)
+df.write.format("mongo").mode("overwrite").save()
+df.show()
+
+ 
+        
